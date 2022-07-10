de470ab107fcd8a9df5bdb556c043b263990ec5b
goohunter <dmitr.goncharov1991@mail.ru>
2022-07-10 14:06:53 +0200

    Installed Slim

--- api/public/index.php
+++ api/public/index.php
@@ -1,7 +1,16 @@
 <?php
 
-//declare(strict_types=1);
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
-header('Content-Type: application/json');
+require __DIR__ . '/../vendor/autoload.php';
 
-echo '{}';
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write('{}');
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
