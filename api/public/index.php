4be58ee60847e2f843db705c615cfd1b083f1825
goohunter <dmitr.goncharov1991@mail.ru>
2022-07-10 14:44:03 +0200

    Installed DI container

--- api/public/index.php
+++ api/public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use DI\Container;
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -9,6 +10,10 @@ http_response_code(500);
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+
+$container = new Container();
+
+$app = AppFactory::createFromContainer($container);
 
 $app->addErrorMiddleware((bool)getenv('APP_DEBUG'), true, true);
 
