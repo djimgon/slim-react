cfd2ce3460010ef19a2a09172c4921dd872db58b
goohunter <dmitr.goncharov1991@mail.ru>
2022-07-11 14:14:16 +0200

    Rewrite to PSR request handler

--- api/public/index.php
+++ api/public/index.php
@@ -1,22 +1,28 @@
 <?php
 
-use DI\Container;
+use App\Http;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Slim\Factory\AppFactory;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 
 http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$builder = new DI\ContainerBuilder();
 
-$container = new Container();
+$builder->addDefinitions([
+    'config' => [
+        'debug' => (bool)getenv('APP_DEBUG'),
+    ],
+    ResponseFactoryInterface::class => Di\get(Slim\Psr7\Factory\ResponseFactory::class),
+]);
+
+$container = $builder->build();
 
 $app = AppFactory::createFromContainer($container);
 
-$app->addErrorMiddleware((bool)getenv('APP_DEBUG'), true, true);
+$app->addErrorMiddleware($container->get('config')['debug'], true, true);
 
-$app->get('/',  \App\Http\Action\HomeAction::class);
+$app->get('/', Http\Action\HomeAction::class);
 
 $app->run();
