26fdff54a7ab6a40a4339de9cc50d0124e8461c2
goohunter <dmitr.goncharov1991@mail.ru>
2022-07-19 12:47:57 +0200

    Splitted database console commands

--- api/bin/app.php
+++ api/bin/app.php
@@ -1,7 +1,10 @@
+#!/usr/bin/env php
 <?php
 
 declare(strict_types=1);
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
@@ -18,6 +21,10 @@ $cli = new Application('Console');
  * @psalm-suppress MixedArrayAccess
  */
 $commands = $container->get('config')['console']['commands'];
+
+/** @var EntityManagerInterface $entityManager */
+$entityManager = $container->get(EntityManagerInterface::class);
+$cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
 
 foreach ($commands as $name) {
     /** @var Command $command */
