6b1fc650a352fb486956b876a9d1da81c676a128
goohunter <dmitr.goncharov1991@mail.ru>
2022-07-26 14:11:22 +0200

    Change routes

--- api/config/routes.php
+++ api/config/routes.php
@@ -9,10 +9,13 @@ use Slim\Routing\RouteCollectorProxy;
 return static function (App $app): void {
     $app->get('/', Action\HomeAction::class);
 
-    $app->group('/v1', function (RouteCollectorProxy $group): void {
-        $group->group('/auth', function (RouteCollectorProxy $group): void {
-            $group->post('/join/confirm', Action\V1\Auth\Join\ConfirmAction::class);
-            $group->post('/join', Action\V1\Auth\Join\RequestAction::class);
-        });
-    });
+    $app->post('/v1/auth/join/', Action\V1\Auth\Join\RequestAction::class);
+    $app->post('/v1/auth/join/confirm/', Action\V1\Auth\Join\ConfirmAction::class);
+
+//    $app->group('/v1', function (RouteCollectorProxy $group): void {
+//        $group->group('/auth', function (RouteCollectorProxy $group): void {
+//            $group->post('/join/confirm', Action\V1\Auth\Join\ConfirmAction::class);
+//            $group->post('/join', Action\V1\Auth\Join\RequestAction::class);
+//        });
+//    });
 };
