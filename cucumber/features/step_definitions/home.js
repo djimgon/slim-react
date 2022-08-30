8ea9a917cb12c0a3e6a2df2fabfe4fb651791d7f
goohunter <dmitr.goncharov1991@mail.ru>
2022-08-30 14:51:52 +0200

    Remove Cucumber js

--- cucumber/features/step_definitions/home.js
+++ cucumber/features/step_definitions/home.js
@@ -1,9 +1,9 @@
 
-const { When, Then } = require('cucumber')
-const { expect } = require('chai')
-
-Then('I see welcome block', async function () {
-  await this.page.waitForSelector('[data-testid=welcome]')
-  const text = await this.page.$eval('[data-testid=welcome] h1', el => el.textContent)
-  expect(text).to.eql('Auction')
-})
+// const { When, Then } = require('cucumber')
+// const { expect } = require('chai')
+//
+// Then('I see welcome block', async function () {
+//   await this.page.waitForSelector('[data-testid=welcome]')
+//   const text = await this.page.$eval('[data-testid=welcome] h1', el => el.textContent)
+//   expect(text).to.eql('Auction')
+// })
--- cucumber/features/support/steps/user.js
+++ cucumber/features/support/steps/user.js
@@ -1,3 +1,3 @@
-const { Given } = require('cucumber')
-
-Given('I am a guest user', function () {})
+// const { Given } = require('cucumber')
+//
+// Given('I am a guest user', function () {})
